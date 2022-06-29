40ed891a18788d00575281891cb79520ba96fb3a
TinNguyen <ductin16111@gmail.com>
2022-06-29 14:12:45 +0300

    verify if the response reveived successfully

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from urllib import response
 import requests
+
 API_KEY = "b92a75a142d591e7c8c6381e8311c403"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
-request_url = f"{BASE_URL} ? appid = {API_KEY} & q = {city}"   # Embed var and expressions inside of a string
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"   # Embed var and expressions inside of a string
 # appid and q are query parameters
 response = requests.get(request_url)    # send a get request to url
 # response contains info associate with our city
+
+if response.status_code == 200: # 200: successful
+    data = response.json()  # data returned as a json/python dictionary
+    print(data)
+else:
+    print("Error")
